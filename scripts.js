e1d86eb7dceedc215b03ead7ecad554ff7e1a629
Aditya Thakkar <63272852+thakkar-stack@users.noreply.github.com>
2023-08-07 17:54:25 +1000

    added strict equality for user inputs

--- scripts.js
+++ scripts.js
@@ -10,34 +10,36 @@ function playRound(playerSelection, computerSelection){
     playerSelection = prompt('Rock(r)?, Paper(p)? or Scissors(s)?:');
     computerSelection = getComputerChoice();
 
-    if(playerSelection === computerSelection){
-        console.log(`${playerSelection} against ${computerSelection} is a tie!`);
-        console.log(`Your score: ${playerScore} Computer score: ${computerScore}`);
-    }
-    else if(playerSelection === 'r' && computerSelection === 's'){
-        console.log(`${playerSelection} against ${computerSelection} you win!`);
-        playerScore++;
-        console.log(`Your score: ${playerScore} Computer score: ${computerScore}`);
-    }
-    else if(playerSelection === 's' && computerSelection === 'p'){
-        console.log(`${playerSelection} against ${computerSelection} you win!`);
-        playerScore++;
-        console.log(`Your score: ${playerScore} Computer score: ${computerScore}`);
-    }
-    else if(playerSelection === 'p' && computerSelection === 'r'){
-        console.log(`${playerSelection} against ${computerSelection} you win!`);
-        playerScore++;
-        console.log(`Your score: ${playerScore} Computer score: ${computerScore}`);
-    }
-    else{
-        console.log(`${playerSelection} against ${computerSelection} you lose!`);
-        computerScore++;
-        console.log(`Your score: ${playerScore} Computer score: ${computerScore}`);
-    }
+    if(playerSelection === 'r' || playerSelection === 'p' || playerSelection === 's'){
+        if(playerSelection === computerSelection){
+            console.log(`${playerSelection} against ${computerSelection} is a tie!`);
+            console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        }
+        else if(playerSelection === 'r' && computerSelection === 's'){
+            console.log(`${playerSelection} against ${computerSelection} you win!`);
+            playerScore++;
+            console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        }
+        else if(playerSelection === 's' && computerSelection === 'p'){
+            console.log(`${playerSelection} against ${computerSelection} you win!`);
+            playerScore++;
+            console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        }
+        else if(playerSelection === 'p' && computerSelection === 'r'){
+            console.log(`${playerSelection} against ${computerSelection} you win!`);
+            playerScore++;
+            console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        }
+        else{
+            console.log(`${playerSelection} against ${computerSelection} you lose!`);
+            computerScore++;
+            console.log(`Your score: ${playerScore}. Computer score: ${computerScore}`);
+        }
+    } else console.log(`${playerSelection} is not an option! Pick again.`)
 }
 
 function playGame(){
-    while (!(playerScore == 5 || computerScore == 5)){
+    while (!(playerScore === 5 || computerScore === 5)){
         playRound();
 
         if(playerScore === 5 || computerScore === 5){
